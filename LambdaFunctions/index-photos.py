b5357f507251e8a2ee19f4cba5260c1ed7e79736
abhishek <abhishek.adinarayan@gmail.com>
2025-04-22 21:02:49 -0400

    Added Lamdba functions

---
+++ LambdaFunctions/index-photos.py
@@ -0,0 +1,90 @@
+import boto3
+import json
+import urllib
+from datetime import datetime
+from opensearchpy import OpenSearch, RequestsHttpConnection
+import os
+import time
+import base64
+
+# Initialize clients
+s3 = boto3.client('s3')
+rekognition = boto3.client('rekognition', region_name='us-east-1')
+
+# ElasticSearch configuration
+es_endpoint = "<ENDPOINT>"
+es_username = "<USERNAME>"
+es_password = "<PASSWORD>"
+
+es = OpenSearch(
+    hosts=[{'host': es_endpoint, 'port': 443}],
+    http_auth=(es_username, es_password),
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+
+def lambda_handler(event, context):
+    # Get the S3 bucket and key from the event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'])
+    # bucket = event['Records'][0]['s3']['bucket']['name']
+    # key = event['Records'][0]['s3']['object']['key']
+
+    print("bucket:", bucket)
+    print("key:", key)
+
+    # Retrieve the object from S3
+    s3_object = s3.get_object(Bucket=bucket, Key=key)
+    object_content = s3_object['Body'].read()
+    decoded_content = base64.b64decode(object_content)
+
+    rekognition_response = rekognition.detect_labels(
+        Image={'Bytes': decoded_content},
+        MaxLabels=10,
+        MinConfidence=80
+    )
+
+    rekognition_labels = [label['Name'] for label in rekognition_response['Labels']]
+    print("rekognition_labels:", rekognition_labels)
+
+    # Extract custom metadata labels
+    metadata = s3.head_object(Bucket=bucket, Key=key)
+    custom_labels = metadata['ResponseMetadata']['HTTPHeaders'].get('x-amz-meta-customlabels', '').split(',')
+    print("custom_labels:", custom_labels)
+    all_labels = custom_labels + rekognition_labels
+
+    print("all_labels:", all_labels)
+
+    if all_labels:
+        # Create document for ElasticSearch
+        document = {
+            "objectKey": key,
+            "bucket": bucket,
+            "createdTimestamp": datetime.now().isoformat(),
+            "labels": all_labels
+        }
+
+        print("Indexing images")
+
+        # Index the document
+        es.index(
+            index="photos",
+            id=key,
+            body=document,
+            refresh=True
+        )
+
+        print("document:", document)
+        print("Indexed document:", json.dumps(document))
+
+    # Update the object in S3 with the original content
+    time.sleep(10)  # Delay to ensure operations are completed
+    s3.delete_object(Bucket=bucket, Key=key)
+    s3.put_object(Bucket=bucket, Body=decoded_content, Key=key, ContentType='image/jpg')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Photo indexed successfully')
+    }
---
+++ LambdaFunctions/search-photos.py
@@ -0,0 +1,95 @@
+import json
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+
+# Initialize Lex client
+lex_client = boto3.client('lexv2-runtime', region_name='us-east-1')
+
+# ElasticSearch configuration
+es_endpoint = "<ENDPOINT>"
+es_username = "<USERNAME>"
+es_password = "<PASSWORD>"
+
+es = OpenSearch(
+    hosts=[{'host': es_endpoint, 'port': 443}],
+    http_auth=(es_username, es_password),
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+# Lex configuration
+BOT_ID = "<BOT_ID>"
+BOT_ALIAS_ID = "<BOT_ALIAS_ID>"
+LOCALE_ID = "en_US"
+
+
+def lambda_handler(event, context):
+    # Extract all slots with resolved values
+
+    print("Event:", event)
+    user_query = event["queryStringParameters"]["q"]
+    print("User query:", user_query)
+
+    lex_response = lex_client.recognize_text(
+        botId=BOT_ID,
+        botAliasId=BOT_ALIAS_ID,
+        localeId=LOCALE_ID,
+        sessionId="1234",
+        text=user_query
+    )
+
+    print("Lex response:", lex_response)
+
+    # Get interpreted values (normalized)
+    slot_info = lex_response.get('sessionState', {}).get('intent', {}).get('slots', {})
+    print("Slot info:", slot_info)
+
+    extracted_labels = []
+
+    for slot_name, slot_data in slot_info.items():
+        if slot_data and 'value' in slot_data:
+            slot_label = slot_data['value']['interpretedValue']
+
+            if slot_label:
+                extracted_labels.append(slot_label)
+
+    print("All labels:", extracted_labels)
+
+    image_metadata = []
+
+    for label in extracted_labels:
+        query = {
+            "query": {
+                "match": {
+                    "labels": label
+                }
+            }
+        }
+
+        try:
+            response = es.search(index="photos", body=query)
+            hits = response['hits']['hits']
+
+            if hits:
+                for hit in hits:
+                    if all(hit['_source']['objectKey'] != item['objectKey'] for item in image_metadata):
+                        image_metadata.append(hit['_source'])
+
+        except Exception as e:
+            print("ElasticSearch error:", str(e))
+            return {"statusCode": 500, "body": "Search failed"}
+
+    print("Image metadata:", image_metadata)
+
+    # Prepare HTTP response
+    return {
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'statusCode': 200,
+        'body': json.dumps(image_metadata)
+    }
+
